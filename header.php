4ac721dc6507ba01cb6923f7ffe5141e860dea10
Teyir <60007549+Teyir@users.noreply.github.com>
2021-02-18 02:40:57 +0100

    Fix header (background) Début de l'opti du css

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <div class="header_banner">
     <div class="banner_inner d-flex align-items-center">
-        <div class="overlay" style="background: url('<?= $_Theme_['Main']['theme']['bg'] ?>')
+        <div class="overlay" style="background: url('theme/upload/bg.png')
             no-repeat scroll center center; background-size: cover; max-height: 150px"></div>
     </div>
 </div>
